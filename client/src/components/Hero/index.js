a76bc2058e4c92b0ef71dd9c090b63f86e3d98c4
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-08 22:08:38 -0500

    Set up the Hero component and the css

---
+++ client/src/components/Hero/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./style.css";
+
+function Hero () {
+    return(
+    <div className="hero">
+        <h1>(React) Google Books Search</h1>
+        <h2>Search for and Save Books of Interest</h2>
+    </div>
+    )
+};
+  
+
+  export default Hero;
+
+
