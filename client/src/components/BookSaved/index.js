0db32030e0fdf373442d397dff8a5b4b7f256d63
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-08 21:56:07 -0500

    Mapping over savedData to return the BookResults

--- client/src/components/BookSaved/index.js
+++ client/src/components/BookSaved/index.js
@@ -37,7 +37,22 @@ render() {
     <div>
     {this.state.saved.length > 0 ? 
     <BookDetail>
-          
+    {this.state.saved.map((savedData) => {
+            console.log(savedData);
+            return (
+              <BookResults
+              info={savedData}
+              key={savedData._id}
+              title={savedData.title}
+              author={savedData.author}
+              description={savedData.description}
+              link={savedData.link}
+              image={savedData.image}
+              buttonText= 'delete'
+              handleClick={this.handleDeletedData(savedData._id)}
+              />
+            );
+          })}    
     </BookDetail>
       : <p>No results</p>
       }
