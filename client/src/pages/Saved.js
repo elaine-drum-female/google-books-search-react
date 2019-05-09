3b75f29a67e32d768bb01075c79f0fb5c92b3646
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-08 22:11:22 -0500

    Added the Saved function with Hero and BookSaved

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import BookSaved from '../components/BookSaved';
+
+function Saved() {
+    return (
+      <div>
+         <Hero/>
+         <BookSaved />
+    </div>
+    );
+  }
+  
+  export default Saved;
+  
