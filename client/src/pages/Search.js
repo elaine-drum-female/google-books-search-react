86b64654d93e36eb5b29e626416ae05ab7a4009e
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-08 22:11:57 -0500

    Added Search function with Hero and BooksContainer

---
+++ client/src/pages/Search.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import BooksContainer from '../components/BooksContainer';
+
+
+function Search() {
+  return (
+    <div>
+      <Hero/>
+      <BooksContainer/>
+    </div>
+    
+  );
+}
+
+export default Search;
+
+
+  
